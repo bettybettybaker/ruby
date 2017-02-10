61565f9f2a03e3f4282293d87af8df2a717c6f10
Betty Baker <betty@heysparkbox.com>
2017-02-10 14:55:32 -0500

    First commit -- ruby question exercise

---
+++ question-exercise.rb
@@ -0,0 +1,55 @@
+def prompt
+  print "\n \n >> "
+  gets.chomp
+  print "\n"
+end
+
+
+print "\n\n\nWhat's your name?"
+name = "#{prompt}"
+
+print "Are you a morning or a night person?"
+time = "#{prompt}"
+
+print "Do you prefer sweet or salty foods?"
+foods = "#{prompt}"
+
+print "Ninjas or pirates?"
+ninjas = "#{prompt}"
+
+print "If you could be an animal, what would you be?"
+animal = "#{prompt}"
+
+print "List 3 of your best personality traits."
+best = "#{prompt}"
+
+print "If you HAD to change your name, what would you change it to?"
+change = "#{prompt}"
+
+print "What is your favorite movie?"
+movie = "#{prompt}"
+
+print "Are you a dog person?"
+doggos = "#{prompt}"
+
+print "How many languages do you speak? (Klingon counts)"
+languages = "#{prompt}"
+
+
+
+puts """
+Hey #{name}, thanks for answering my questions.
+When it comes to being a morning or a night person, you chose #{time}.
+Good to know.
+You prefer #{foods} foods. I can relate. Meee tooo.
+It's a good thing you chose #{ninjas}.
+If you could be an animal, you would be a #{animal}.
+Three of your best personality traits are that you are: #{best}
+I think we can all agree on that one.
+If you HAD to change your name, you would be called #{change}.
+Your favorite movie, dear #{change}, is apparently #{movie}.
+I can see why. Cool flick.
+So the doggos? You a fan of the doggos? You said #{doggos}.
+When it comes to languages, I speak one. You speak #{languages}.
+Thank goodness we both speak at least one in common!
+"""
