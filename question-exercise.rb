8821bb11399004fd70a9d63ff60d38b355d8e362
Betty Baker <betty@heysparkbox.com>
2017-03-01 16:35:28 -0500

    :racehorse: feat: Yock's Challenge WIP continued

--- question-exercise.rb
+++ question-exercise.rb
@@ -12,10 +12,11 @@ class Question
   def ask
     puts @question
   end
-  def prompt
-    gets.chomp
-  end
+   def answer
+     gets.chomp
+   end
 end
+
 
 def questions
                                   # YOCK'S HINT: question instance here? (demo in :name)
@@ -23,39 +24,14 @@ def questions
     name: Question.new("What's your name?")
   }
 end
-  #  tod: "Are you a morning or a night person?",
-  #  foods: "Do you prefer sweet or salty foods?",
-  #  ninjas: "Ninjas or Pirates?",
-  #  animal: "If you could be an animal, what would you be?",
-  #  best: "What are three of your best personality traits?",
-  #  change: "If you HAD to change your name, what would you change it to?",
-  #  movie: "What is your favorite movie?",
-  #  doggos: "Are you a dog person?",
-  #  languages: "How many languages do you speak? (Klingon counts)"
-#  }
-#end
+
 
 
 questions.each do |question|
-  Question.ask
-  Question.prompt
+  question.ask
+  question.answer
 end
 
 puts """
 Hey #{questions[:name].answer}, thanks for answering my questions.
 """
-=begin
-When it comes to being a morning or a night person, you chose #{answers[:tod]}.
-Good to know.
-You prefer #{answers[:foods]} foods. I can relate. Meee tooo.
-It's a good thing you chose #{answers[:ninjas]} (anything else is just wrong).
-If you could be an animal, you would be a #{answers[:animal]}.
-Three of your best personality traits are that you are: #{answers[:best]}.
-If you HAD to change your name, you would be called #{answers[:change]}.
-Your favorite movie, dear #{answers[:change]}, is apparently #{answers[:movie]}.
-I can see why. Cool flick.
-So the doggos? You a fan of the doggos? You said #{answers[:doggos]}.
-When it comes to languages, I speak one. You speak #{answers[:languages]}.
-Thank goodness we both speak at least one in common!
-"""
-=end
