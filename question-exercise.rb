c973486e60105de8f911d62de3f0ab89151ce3f9
Betty Baker <betty@heysparkbox.com>
2017-03-01 16:08:02 -0500

    :racehorse: feat: Attempt Yock's challenge

--- question-exercise.rb
+++ question-exercise.rb
@@ -20,29 +20,29 @@ end
 def questions
                                   # YOCK'S HINT: question instance here? (demo in :name)
   {
-    name: Question.new "What's your name?",
-    tod: "Are you a morning or a night person?",
-    foods: "Do you prefer sweet or salty foods?",
-    ninjas: "Ninjas or Pirates?",
-    animal: "If you could be an animal, what would you be?",
-    best: "What are three of your best personality traits?",
-    change: "If you HAD to change your name, what would you change it to?",
-    movie: "What is your favorite movie?",
-    doggos: "Are you a dog person?",
-    languages: "How many languages do you speak? (Klingon counts)"
+    name: Question.new "What's your name?"
+  #  tod: "Are you a morning or a night person?",
+  #  foods: "Do you prefer sweet or salty foods?",
+  #  ninjas: "Ninjas or Pirates?",
+  #  animal: "If you could be an animal, what would you be?",
+  #  best: "What are three of your best personality traits?",
+  #  change: "If you HAD to change your name, what would you change it to?",
+  #  movie: "What is your favorite movie?",
+  #  doggos: "Are you a dog person?",
+  #  languages: "How many languages do you speak? (Klingon counts)"
   }
 end
 
 
-answers = {}   # try to get rid of this
-
-questions.each do |label, question|
+questions.each do |question|
   question.ask
-  question.answer = question.prompt
+  question.prompt
 end
 
 puts """
 Hey #{questions[:name].answer}, thanks for answering my questions.
+"""
+=begin
 When it comes to being a morning or a night person, you chose #{answers[:tod]}.
 Good to know.
 You prefer #{answers[:foods]} foods. I can relate. Meee tooo.
@@ -56,3 +56,4 @@ So the doggos? You a fan of the doggos? You said #{answers[:doggos]}.
 When it comes to languages, I speak one. You speak #{answers[:languages]}.
 Thank goodness we both speak at least one in common!
 """
+=end
