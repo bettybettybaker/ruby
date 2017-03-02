79a4c507902b55e59f6bb3c3abed68cbd8378eb3
Betty Baker <betty@heysparkbox.com>
2017-03-02 14:47:47 -0500

    :racehorse: feat: Commit number eleven

--- question-exercise.rb
+++ question-exercise.rb
@@ -19,7 +19,16 @@ end
 
 
 questions = {
-  name: Question.new("What's your name?")
+  name: Question.new("What's your name?"),
+  tod: Question.new("Are you a morning or a night person?"),
+  foods: Question.new("Do you prefer sweet or salty foods?"),
+  ninjas: Question.new("Ninjas or Pirates?"),
+  animal: Question.new("If you could be an animal, what would you be?"),
+  best: Question.new("What are three of your best personality traits?"),
+  change: Question.new("If you HAD to change your name, what would you change it to?"),
+  movie: Question.new("What is your favorite movie?"),
+  doggos: Question.new("Are you a dog person?"),
+  languages: Question.new("How many languages do you speak? (Klingon counts)")
 }
 
 
@@ -31,4 +40,16 @@ end
 
 puts """
 Hey #{questions[:name].answer}, thanks for answering my questions.
+When it comes to being a morning or a night person, you chose #{questions[:tod].answer}.
+Good to know.
+You prefer #{questions[:foods].answer} foods. I can relate. Meee tooo.
+It's a good thing you chose #{questions[:ninjas].answer} (anything else is just wrong).
+If you could be an animal, you would be a #{questions[:animal].answer}.
+Three of your best personality traits are that you are: #{questions[:best].answer}.
+If you HAD to change your name, you would be called #{questions[:change].answer}.
+Your favorite movie, dear #{questions[:change].answer}, is apparently #{questions[:movie].answer}.
+I can see why. Cool flick.
+So the doggos? You a fan of the doggos? You said #{questions[:doggos].answer}.
+When it comes to languages, I speak one. You speak #{questions[:languages].answer}.
+Thank goodness we both speak at least one in common!
 """
